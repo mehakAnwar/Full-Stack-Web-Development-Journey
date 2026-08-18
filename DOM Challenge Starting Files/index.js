7ef87e7644bcedb169a65227a235cfa04cc40644
mehakAnwar <mehakAnwar@users.noreply.github.com>
2026-08-19 04:27:23 +0500

    ADVANCE JAVASCRIPT PRACTICE

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -1,1 +1,25 @@
 document.querySelector("h1").classList.add("huge");
+
+
+/*
+------------------------------OBJECTS & FUNCTIONS & DOT NOTATION--------------------
+
+
+function HouseKeepers(name,age,experience,language){            => constructor function
+    this.name=name;
+    this.age=age;
+    this.experience= experience;
+    this.language=language;
+    this.clean = function(){
+        alert("cleaning in progress");
+    }
+}
+    --------------------------OBJECTS---------------------------------
+
+var houseKeeperOne = new HouseKeepers("Meera",21,"6years",["english","tamil","hindi"]);
+var houseKeeperTwo = new HouseKeepers("Teena",21,"8years",["english","marathi","hindi"]);
+var houseKeeperThree = new HouseKeepers("Meena",21,"4years",["malay","tamil","hindi"]);
+var houseKeeperFour = new HouseKeepers("Meena",21,"4years",["malay","tamil","hindi"]);
+var houseKeeper5 = new HouseKeepers("beena",21,"4years",["malay","tamil","hindi"]);
+
+console.log(houseKeeper5.clean()); */
