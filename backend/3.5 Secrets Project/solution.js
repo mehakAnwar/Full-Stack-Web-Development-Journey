72fa94a33d9017e2abc6d6636999091e663b04ae
mehakAnwar <mehakAnwar@users.noreply.github.com>
2026-08-27 05:18:43 +0500

    Completed Secret Access Project

--- backend/3.5 Secrets Project/solution.js
+++ backend/3.5 Secrets Project/solution.js
@@ -2,28 +2,35 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 5000;
-var bandName = "";
+const port = 3000;
+
+var userIsAuthorised = false;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function bandNameGen(req, res, next) {
-  bandName = req.body["street"] + req.body["pet"];
+function passwordCheck(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    userIsAuthorised = true;
+  }
   next();
 }
-
-app.use(bandNameGen);
+app.use(passwordCheck);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post("/submit", (req, res) => {
-  res.send(`<h1>Your band name is:</h1><h4>${bandName}</h4>`);
+app.post("/check", (req, res) => {
+  if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+    //Alternatively res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
